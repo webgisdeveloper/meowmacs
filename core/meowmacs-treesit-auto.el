f482572b71bbdeae86c560816244636b804a6735
webgisdeveloper <webgisdeveloper@users.noreply.github.com>
2025-11-06 09:48:37 -0500

    #1 debug

--- core/meowmacs-treesit-auto.el
+++ core/meowmacs-treesit-auto.el
@@ -1,10 +1,17 @@
 ;;; meowmacs-treesit-auto.el -*- lexical-binding: t; -*-
 
+;;; Commentary:
+;; treesit-auto configuration for automatic tree-sitter mode selection
+
+;;; Code:
+
 (use-package treesit-auto
   :ensure t
+  :demand t  ;; Load immediately, don't defer
   :config
+  (setq treesit-auto-install 'prompt)  ;; Prompt before installing grammars
   (global-treesit-auto-mode))
 
 (provide 'meowmacs-treesit-auto)
 
-;;; meowmacs-treesit-auto.el-treesitter.el ends here
+;;; meowmacs-treesit-auto.el ends here
